2a9e43810a99f04def5e9a758486fbddb1174216
Rafael dos Santos <rafaels1997@LIVE.COM>
2023-10-14 00:47:29 -0300

    Adição de input para inserir os comandos

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,18 +1,29 @@
 import gals.*;
 
 import java.io.StringReader;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws LexicalError, SemanticError, SyntaticError {
 
-        try {
-            Lexico lexico = new Lexico(new StringReader("A = 10; B = 11; B = 111 + A * B; imprimir ( B );"));
-            Sintatico sintatico = new Sintatico();
-            Semantico semantico = new Semantico();
+        Scanner input = new Scanner(System.in);
+        String comando;
 
-            sintatico.parse(lexico, semantico);
-        }catch (LexicalError | SyntaticError | SemanticError e){
-            System.out.println("comando nao encontrado");
-        }
+        do {
+
+            System.out.print("Digite um comando: ");
+            comando = input.nextLine();
+
+            try {
+                Lexico lexico = new Lexico(new StringReader(comando));
+                //Lexico lexico = new Lexico(new StringReader("A = 10; B = 11; B = 111 + A * B; imprimir ( B );"));
+                Sintatico sintatico = new Sintatico();
+                Semantico semantico = new Semantico();
+
+                sintatico.parse(lexico, semantico);
+            } catch (LexicalError | SyntaticError | SemanticError e) {
+                System.out.println("comando nao encontrado");
+            }
+        } while (true);
     }
 }
